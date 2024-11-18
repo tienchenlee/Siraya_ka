2aa76be3c5fddb95988207b061862962f245f526
tienchenlee <appielee4305@gmail.com>
2024-11-18 15:25:29 +0800

    using docx

--- workspace/tool/extracted_ka.py
+++ workspace/tool/extracted_ka.py
@@ -1,82 +1,74 @@
 #!/user/bin/env python
 # -*- coding: utf-8 -*-
 
-import re
 import json
+import os
+import re
 
-from bs4 import BeautifulSoup
+from docx import Document
 from pprint import pprint  
 
-def main(htmlSTR):
-    soup = BeautifulSoup(htmlSTR, "lxml")
-    sirayaDICT = {}
-    sentenceDICT = {}
-    
-    divLIST = soup.select('div[style="padding: 7px;"]')  
-    spansLIST = []
-    for d in divLIST:
-        spans = d.find_all("span")
-        spansLIST.append([spans[0].text.strip(), spans[1].text.strip()])
+folder_path = r"Gospel of Matthew, 2024.9.03"
+def order_file(file_name):
+    match = re.search(r"\d+", file_name)
+    return int(match.group())
 
-    spans_index = 0  # 用於跟蹤 spansLIST 的起始位置
-    h3_LIST = soup.find_all('h3')        
-    for h3 in h3_LIST:
-        h3STR = h3.get_text()
-        cleaned_h3STR = h3STR.replace("\t", "").replace("\n", " ").strip()
-        sentences = re.split(r"([\.?])", cleaned_h3STR)
-        if len(sentences) == 1 and sentences[0] == cleaned_h3STR:  # 檢查是否為空列表
-            cleaned_sentence = [cleaned_h3STR]  # 使用原始字符串
-        else:
-            cleaned_sentence = [(sentences[i] + sentences[i + 1]).strip() for i in range(0, len(sentences) - 1, 2)]        
-        
-        #sentences = re.split(r"([\.?])", cleaned_h3STR)
-        #cleaned_sentence = [(sentences[i] + sentences[i + 1]).strip() for i in range(0, len(sentences) - 1, 2)]
-        #print(cleaned_sentence)
-        
-        sentenceLIST = []
-        for s in cleaned_sentence:
-            sLIST = list(s.split())  # 為每個句子建立 sLIST
-            tmpLIST = []
-    
-            for word in sLIST:
-                cleaned_word = re.sub("-", "", word)
-                if spans_index < len(spansLIST):
-                    span = spansLIST[spans_index]
-                    cleaned_span = re.sub("-", "", span[0])
-                    match = re.search(r"\w+", cleaned_word, re.IGNORECASE)
-                    if match and match.group().lower() == cleaned_span.lower():
-                        tmpLIST.append(span[1])
-                    spans_index += 1 
-    
-            tmpSTR = " ".join(tmpLIST)
-            sentenceLIST.append({s: tmpSTR}) # 將翻譯結果儲存到當前句子
-        print(sentenceLIST) 
-    
-    #h1_LIST = soup.find_all('h1', class_="secBG")
-    #for h1 in h1_LIST:
-        #h1STR = h1.get_text()
-        #match = re.search(r"\d+:\d+", h1STR)
-        #if match:
-            #key = match.group()
-            #sirayaDICT[key] = sentenceLIST    
-    
-    ##if key and value:
-        ##sirayaDICT[key]= value
-    #return sirayaDICT    
-    
-#def extracted_ka():
-       
+def docx_to_txt(docx_path, txt_path):
+    # 讀取 Word 文件
+    document = Document(docx_path)
 
+    # 開啟並寫入文字文件
+    with open(txt_path, 'w', encoding='utf-8') as txt:
+        for paragraph in document.paragraphs:
+            # 去除段落中的空格並寫入文本文件
+            #clean_paragraph = re.sub(" ", "", paragraph.text)
+            txt.write(paragraph.text + '\n')
+
+def mktxt_files():
+    docFILES = [f for f in os.listdir(folder_path) if f.endswith(".docx")]
+    sorted_docFILES = sorted(docFILES, key=lambda f: order_file(f))
+    
+    for file in sorted_docFILES:
+        file_path = os.path.join(folder_path, file)
+        output_txt_path = os.path.join(folder_path, os.path.splitext(file)[0] + ".txt")
+        docx_to_txt(file_path, output_txt_path)
+        
+def extracted_ka():
+    txtFILES = [f for f in os.listdir(folder_path) if f.endswith(".txt")]
+    sorted_txtFILES = sorted(txtFILES, key=lambda f: order_file(f))
+    
+    #kaDICT = {} 
+    for file in sorted_txtFILES:
+        file_path = os.path.join(folder_path, file)
+        with open(file_path, "r", encoding="utf-8") as txt:
+            content = txt.read()
+            #看一下內容長怎樣
+            #print(content)
+            
+            del_chinese = re.sub(r"[\u4e00-\u9fff]+", "", content)
+            print(del_chinese)
+            #sentences = re.findall(r"[a-zA-Z\.,:;-?].+\t[a-zA-Z\.,:;-?]+-?[a-zA-Z\.,:;?]+", content)
+            #chapters = re.findall(r"(?<=[.?:;]\n)\d+:\d+", content)
+            #print(sentences)
+            
+            #for ch in sorted(set(chapters), key=lambda x: tuple(map(int, x.split(":")))):            
+                #print(f"Gloss sentences for '{ch}':")
+
+            
+    
+    
 
 if __name__ == "__main__":
-    html_files = {
-        "./Matthew Website.html": "ka_in_Matthew.json",
-        "./John Website.html": "ka_in_John.json"
-    }
-    for htmlFILE, jsonFILE in html_files.items():
-        with open(htmlFILE, "r", encoding="utf-8") as web:
-            htmlSTR = web.read()
-            result = main(htmlSTR)
+    mktxt_files()
+    extracted_ka()
+    #html_files = {
+        #"./Matthew Website.html": "ka_in_Matthew.json",
+        #"./John Website.html": "ka_in_John.json"
+    #}
+    #for htmlFILE, jsonFILE in html_files.items():
+        #with open(htmlFILE, "r", encoding="utf-8") as web:
+            #htmlSTR = web.read()
+            #result = main(htmlSTR)
         
     #with open(jsonFILE, "w", encoding="utf-8") as js:
         #json.dump(sentenceDICT, js, ensure_ascii=False, indent=4)     
