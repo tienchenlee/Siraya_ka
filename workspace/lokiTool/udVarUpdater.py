62c2cb4ad10c64593c1511dab52313516ff44d1b
tienchenlee <appielee4305@gmail.com>
2025-11-16 23:17:58 +0800

    update regex var and updater

---
+++ workspace/lokiTool/udVarUpdater.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import json
+from requests import post
+from pathlib import Path
+
+accountPATH = Path.cwd() / "account.info"
+with open(accountPATH, "r", encoding="utf-8") as f:
+    accountDICT = json.load(f)
+
+def updateRegexVar():
+    """
+    Align regex var in three projects (Relativizer, Complementizer, Coordinator).
+    """
+    varPATH = Path.cwd().parent.parent / "data" / "VARIABLE.json"
+    with open(varPATH, "r", encoding="utf-8") as f:
+        varDICT = json.load(f)
+
+    url = "https://nlu.droidtown.co/Loki_EN/Call/"
+    loki_keys = ["COMP_loki_key", "cord_loki_key", "REL_loki_key"]
+    for loki_key in loki_keys:
+        payload = {
+          "username": accountDICT["username"],
+          "loki_key": accountDICT[loki_key],
+          # Reset Var
+          "func": "reset_var",
+          "data": {},
+          # Update var
+          "func": "update_var",
+          "data": {
+              "var": varDICT
+          }
+        }
+
+        resultDICT = post(url, json=payload).json()
+        print(f"Update regex Var with {loki_key}:")
+        print(resultDICT)
+        print()
+
+def updateUd():
+    """
+    Align ud in three projects (Relativizer, Complementizer, Coordinator).
+    """
+    udPATH = Path.cwd().parent.parent / "data" / "userDefined.json"
+    with open(udPATH, "r", encoding="utf-8") as f:
+        udDICT = json.load(f)
+
+    url = "https://nlu.droidtown.co/Loki_EN/Call/"
+    loki_keys = ["COMP_loki_key", "cord_loki_key", "REL_loki_key"]
+    for loki_key in loki_keys:
+
+        payload = {
+            "username": accountDICT["username"],
+            "loki_key": accountDICT[loki_key],
+            "func": "update_userdefined",
+            "data": {
+                "user_defined": udDICT
+            }
+        }
+
+        resultDICT = post(url, json=payload).json()
+        print(f"Update UD with {loki_key}:")
+        print(resultDICT)
+        print()
+
+if __name__ == "__main__":
+    updateRegexVar()
+    updateUd()
