e15dbe4d9ed705e556ec94fdd446d9ff86a9cdb5
tienchenlee <appielee4305@gmail.com>
2025-12-11 18:20:45 +0800

    rename Loki_template

---
+++ workspace/Loki_and/and_identifier.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import json
+import logging
+from Coordinator.main import askLoki
+from preLokiTool import udFilter
+from pathlib import Path
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",        # 格式：時間 - 等級 - 訊息
+    handlers=[
+        logging.FileHandler(Path.cwd() / "and_identifier.log", encoding="utf-8", mode="w"),
+        logging.StreamHandler()
+    ]
+)
+
+def main(inputSTR, utterIdx):
+    """"""
+    refDICT = {"inputSTR": [inputSTR],
+               "and": [],
+               "ka_index": [],
+               "utter_index": [utterIdx]}
+
+
+    resultDICT = askLoki(inputSTR, refDICT=refDICT)
+    return resultDICT
+
+if __name__ == "__main__":
+    kaPATH = Path.cwd().parent.parent / "data" / "kaLIST.json"
+    with open(kaPATH, "r", encoding="utf-8") as f:
+        kaLIST = json.load(f)
+
+    resultLIST = []
+    for utterIdx, inputSTR in enumerate(kaLIST[:400]):
+        resultDICT = main(inputSTR, utterIdx)
+        print(resultDICT)
+        print()
+        resultLIST.append(resultDICT)
+
+    #print(resultLIST)
+
+    predictionDIR = Path.cwd().parent.parent / "data" / "training"
+    predictionDIR.mkdir(exist_ok=True, parents=True)
+
+    with open(predictionDIR / "and.json", "w", encoding="utf-8") as f:
+        json.dump(resultLIST, f, ensure_ascii=False, indent=4)
+
+    #<單筆測試>
+    #inputSTR ="when ka see-LV they.GEN NOM star then PAST-joyful.AV NOM they OBL joy ka exceeding.AV great.AV"
+
+    #filterSTR = udFilter(inputSTR)
+    #resultDICT = main(filterSTR, 0)  # 預設句子 index = 0
+    #print(resultDICT)
+    #</單筆測試>
