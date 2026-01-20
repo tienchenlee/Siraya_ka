0f047e77d75f09bc2375819111064a95d5f25f79
tienchenlee <appielee4305@gmail.com>
2026-01-20 10:05:35 +0800

    刪除棄用的 code

--- workspace/Loki_COMP/COMP_identifier.py
+++ workspace/Loki_COMP/COMP_identifier.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-import json
-import logging
 from Loki_COMP.Complementizer.main import askLoki
 from preLokiTool import udFilter
-from pathlib import Path
-from time import sleep
 
 def main(inputSTR, utterIdx):
     """"""
@@ -22,39 +18,6 @@ def main(inputSTR, utterIdx):
     return resultDICT
 
 if __name__ == "__main__":
-    #kaPATH = Path.cwd().parent.parent / "data" / "kaLIST.json"
-    #with open(kaPATH, "r", encoding="utf-8") as f:
-        #kaLIST = json.load(f)
-
-    #resultLIST = []
-    #for utterIdx, inputSTR in enumerate(kaLIST[:400]):
-        #attempts = 0
-        #success = False
-
-        #while attempts < 3 and not success:
-            #resultDICT = main(inputSTR, utterIdx)
-            #print(resultDICT)
-            #print()
-            #sleep(0.8)
-
-            #if "status" in resultDICT.keys():
-                #attempts += 1
-                #logging.warning(f"第 {attempts} 次嘗試: {resultDICT}")
-            #else:
-                #resultLIST.append(resultDICT)
-                #success = True
-
-        #if not success:
-            #logging.error(f"連續 3 次嘗試失敗，跳過此句: {resultDICT}")
-
-    ##print(resultLIST)
-
-    #predictionDIR = Path.cwd().parent.parent / "data" / "training"
-    #predictionDIR.mkdir(exist_ok=True, parents=True)
-
-    #with open(predictionDIR / "COMP.json", "w", encoding="utf-8") as f:
-        #json.dump(resultLIST, f, ensure_ascii=False, indent=4)
-
     #<單筆測試>
     inputSTR = "ka again -PV I .GEN you .PL .NOM speak .AV ka more .AV be.able NOM large.animal ka heavy .AV OBL back go.through .AV LOC hole OBL needle ka enter .AV -IRR NOM rich .AV LOC kingdom OBL God"
 
--- workspace/Loki_REL/REL_identifier.py
+++ workspace/Loki_REL/REL_identifier.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-import json
-import logging
 from Loki_REL.Relativizer.main import askLoki
 from preLokiTool import udFilter
-from pathlib import Path
 
 def main(inputSTR, utterIdx):
     """"""
@@ -19,25 +16,6 @@ def main(inputSTR, utterIdx):
     return resultDICT
 
 if __name__ == "__main__":
-    #kaPATH = Path.cwd().parent.parent / "data" / "kaLIST.json"
-    #with open(kaPATH, "r", encoding="utf-8") as f:
-        #kaLIST = json.load(f)
-
-    #resultLIST = []
-    #for utterIdx, inputSTR in enumerate(kaLIST[:400]):
-        #resultDICT = main(inputSTR, utterIdx)
-        #print(resultDICT)
-        #print()
-        #resultLIST.append(resultDICT)
-
-    ##print(resultLIST)
-
-    #predictionDIR = Path.cwd().parent.parent / "data" / "training"
-    #predictionDIR.mkdir(exist_ok=True, parents=True)
-
-    #with open(predictionDIR / "REL.json", "w", encoding="utf-8") as f:
-        #json.dump(resultLIST, f, ensure_ascii=False, indent=4)
-
     #<單筆測試>
     inputSTR ="PAST- say .AV NOM they him -OBL why ka PAST- instruct -PV GEN Moses ka give -IV .IRR NOM scripture OBL divorcement ka cause.leave -PV .IRR NOM she ka wife OBL man"
 
