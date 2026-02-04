eae09550a8d33ccb6cf9cf713f0d2c955ebccaba
tienchenlee <appielee4305@gmail.com>
2026-02-04 15:33:06 +0800

    not import preLokiTool

--- workspace/Loki_REL/REL_identifier.py
+++ workspace/Loki_REL/REL_identifier.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 from Loki_REL.Relativizer.main import askLoki
-from preLokiTool import udFilter
+#from preLokiTool import udFilter
 
 def main(inputSTR, utterIdx):
     """"""
